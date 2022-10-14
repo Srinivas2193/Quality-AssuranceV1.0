6cf76617b82dc2f8903bc3e723e4f746c4436a1e
Srinivas <101555747+Srinivas2193@users.noreply.github.com>
2022-10-14 20:51:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    screens: {
+      xs: '400px',
+      sm: '793px',
+      md: '893px',
+      lg: '1123px',
+      xl: '1297px',
+      '5xl': '1298px',
+      '1xl': '1362px',
+      '1.5xl': '1366px',
+      '2xl': '1416px',
+      '13mac': '1440px',
+      '3xl': '1616px',
+      '4xl': '1920px'
+    }
+    ,
+
+    extend: {},
+  },
+  plugins: [],
+}
